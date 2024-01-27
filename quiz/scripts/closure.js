695c50ded8a976e87b8b645d75f1369a75dec1d1
Nick TIetje <tietje.n@northeastern.edu>
2024-01-27 13:08:09 -0500

    Finished quiz assignment

--- quiz/scripts/closure.js
+++ quiz/scripts/closure.js
@@ -1,13 +1,14 @@
 function setColor(set) {
         let changeColor = set;
         if(changeColor) {
-            let userColor = document.getElementById('color').value;
+            var userColor = document.getElementById('color').value;
             document.getElementById('myPara').style.color = userColor;
         }
+        console.log("set color to: " + userColor);
         
 }
 
 window.onload = function() {
     let toggle = true;
-    document.getElementById('btn').onclick = setColor(toggle);
+    document.getElementById('btn').onclick = () => { setColor(toggle) };
 }
--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,32 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function createEditNode(onEdit) {
+  let editButton = document.createElement("button");
+  editButton.innerHTML = "Edit Text";
+  editButton.addEventListener('click', () => {
+    onEdit()
+  });
+  return editButton;
+
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let onEdit = () => {
+      let input = document.createElement("input");
+      input.type = "Enter Cell (x,y)";
+      input.value = col1.textContent;
+      col1.textContent = '';
+      col1.appendChild(input);
+      input.focus();
+      input.addEventListener('blur', () => {
+      col1.textContent = input.value;
+    }); };
+    let col2 = createEditNode(() => { onEdit() });
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
